d0ac64ed547c213c935dd62a574a4536662154b8
Rodgers Sande <rd_sande@yahoo.com>
2019-08-13 06:14:22 -0700

    Added the project online

---
+++ public/js/slides.js
@@ -0,0 +1,72 @@
+/*EVENTI IN EVIDENZA */
+$('.owl-one').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+      }
+    })
+
+/*CATEGORIE */
+    $('.owl-two').owlCarousel({
+        loop:true,
+        margin:10,
+        nav:true,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:2
+            },
+            1000:{
+                items:3
+            }
+        }
+    });
+
+/*TESTIMONIANZE */
+
+    $('.owl-testimonianze').owlCarousel({
+        loop:true,
+        margin:10,
+        nav:true,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:2
+            },
+            1000:{
+                items:4
+            }
+        }
+    });
+
+    /*PARLANO */
+    $('.owl-parlano').owlCarousel({
+        loop:true,
+        margin:10,
+        nav:true,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:2
+            },
+            1000:{
+                items:4
+            }
+        }
+    });
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,415 @@
+<!DOCTYPE html>
+<html lang="it">
+
+<head>
+
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+  <title>Equiticket - Buy and Sell tickets at the best price</title>
+
+  <link rel="shortcut icon" type="image/png" href="/img/favicon.png"/>
+
+  <!-- Styles-->
+  <link href="{{asset('css/bootstrap.min.css')}}" rel="stylesheet">
+  <link href="{{asset('css/style.css')}}" rel="stylesheet">
+  <link rel="stylesheet" href="{{asset('css/owl.carousel.min.css')}}">
+  <link rel="stylesheet" href="{{asset('css/owl.theme.default.min.css')}}">
+
+  <!-- Scripts -->
+  <script src="https://use.fontawesome.com/c733b6ace3.js"></script>
+
+</head>
+
+<body>
+
+  <!-- Navigation -->
+  <nav class="topnav navbar-expand-lg static-top">
+    <div class="container">
+      
+      <div class="collapse navbar-collapse" id="navbarResponsive">
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item">
+            <a class="nav-link" href="#"><i class="fa fa-shopping-cart"></i> 0 </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#"><i class="fa fa-eur"></i> 0.00</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+
+  <!-- Page Content -->
+  <div class="eq-hero">
+    <!-- Menu -->
+  <nav class="navbar navbar-expand-lg static-top">
+      <div class="container">
+        <a class="navbar-brand" href="#"><img class="logo" src="{{asset('img/logo.png')}}"></a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item">
+              <button type="button" class="btn color-alternative btn-type-1">Accedi</button>
+            </li>
+            <li class="nav-item">
+              <button type="button" class="btn color-primary btn-type-2">Vendi Biglietti</button>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+
+    <!--Search section -->
+    <div id="container1">
+      
+      <div class="eq-search-bar">
+       <div class="col-md-12 text-center">
+        <div class="input-group">
+            <input type="text" class="form-control" placeholder="Cerca performer, Citta, Evento">
+            <div class="input-group-append">
+              <button class="btn btn-secondary" type="button">
+                <i class="fa fa-search"></i>
+              </button>
+            </div>
+          </div>
+          <div class="eqsearchtitle">
+            <h5 class="eqsearchtitletext">COMBATTI IL BAGARINAGGIO: COMPRA EQUITICKET!</h5>
+          </div>
+        </div>
+      </div>
+
+      <!-- Hero Box -->
+      <div class="col-md-12">
+        <div class="eq-caption-box">
+          <div class="eq-caption-title">
+             <h3 class="eqtitletext"> NESSUN BIGLIETTO <span class="span-type-1">A PREZZO SUPERIORE,</span> ALTRIMENTI È  <span class="span-type-1">GRATIS.</span> </h3>
+             <button type="button" class="btn btn-type-1 color-secondary btn-inline-1">Scopri di piu</button>
+            </div>
+          <div class="eq-hero-caption-body">
+             
+               <div class="col-md-8">
+                 <h4 class="eqcaptionbdtitle"> Oggi il prezzo medio è più basso del</h4>
+               </div>
+               <div class="col-md-4 capside">
+                 <h1 class="eqcaptionside"> 56 <span class="span-type-2">%</span> </h1>
+               </div>
+             
+          </div>
+        </div>
+      </div>
+    </div>
+
+  </div>
+
+  <!-- 2 COLS SECTION -->
+  <div id="container2">
+    <div class="col-md-12">
+    <div class="row eq-blank">
+      <div class="col-md-6 section-left eq-shadow">
+        <div class="section-content">
+          <div class="eq-sc-head">
+            <h5 class="eq-sc-title">HAI COMPRATO UN BIGLIETTO MA NON PUOI PIÙ ANDARE?<span class="eq-sc-title-span">RIVENDILO SU EQUITICKET!</span></h5>
+          </div>
+          <div class="eq-sc-body">
+              <p class="eq-sc-p">La caratteristica di un Equiticket è che il suo prezzo di vendita al massimo è uguale a quello originale oppure è più economico! Combatti assieme a noi il fenomeno del bagarinaggio ed entra nel mondo Equiticket.</p>
+              <button type="button" class="btn btn-type-1 color-primary-alt btn-inline-1">Vendi Biglietti</button>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-6 section-right eq-shadow">
+          <div class="section-content">
+              <div class="eq-sc-head">
+                <h5 class="eq-sc-title">SEI UN ORGANIZZATORE? <span class="eq-sc-title-span">METTI I TUOI BIGLIETTI 
+                  IN VENDITA SU EQUITICKET!</span></h5>
+              </div>
+              <div class="eq-sc-body">
+                  <p class="eq-sc-p">La caratteristica di un Equiticket è che il suo prezzo di vendita al massimo è uguale a quello originale oppure è più economico! Combatti assieme a noi il fenomeno del bagarinaggio ed entra nel mondo Equiticket.</p>
+                  <button type="button" class="btn btn-type-1 color-primary-alt btn-inline-1">Vendi Biglietti</button>
+              </div>
+            </div>
+      </div>
+    </div>
+    </div>
+  </div>
+
+  <!-- EVENTI IN EVIDENZA SECTION -->
+  <div id="container2">
+    <div class="section-heading">
+      <div class="row">
+          <div class="col-md-12">
+             <h2 class="section-title"> Eventi in evidenza </h2>
+          </div>
+      </div>
+    </div>
+
+    <div class="section-content-box eq-shadow">
+      <div class="eq-content-carousel-1 clearfix">
+        <div class="row">
+            <div class="col-md-12">
+              <div class="owl-one owl-carousel owl-theme">
+                <!-- SLIDE Content-->
+                <div> 
+                  <div class="row">
+                    <div class="col-md-6 eq-with-black">
+                      <div class="row">
+                        <div class="col-md-12 eq-sld-poster">
+                            <img src="img/event.png">
+                        </div>
+                      </div>
+                      <div class="row eq-sld-right">
+                          <div class="col-md-6 eq-sld-price">
+                              <span class="span-currency">a partile da</span><h3 class="eq-sc-title-span"><i class="fa fa-eur"></i> 22.0 </h3>
+                          </div>
+                          <div class="col-md-6 eq-sld-btn">
+                            <button type="button" class="btn btn-type-1 color-primary">Acquista</button>
+                          </div>
+                        </div>                        
+                    </div>
+                    <div class="col-md-6">
+                      <div class="eq-sld-eventdesc">
+                        <div class="eq-sld-eventdesc-title">
+                          <h4> <span class="eq-event-name">Metroman</span> <span class="eq-event-name">Roberto Genovese DJ </span></h4>
+                        </div>
+                        <div class="eq-sld-eventdesc-parag">
+                            <p>Balla al ritmo dei successi anni 90 e 2000. Muovi i fianchi e divertiti con i successi degli anni passati... Per veri nostalgici!
+                              PISTA 2 HOT IN HERE /hip hop/reggaeton/trap
+                              PISTA 3 Techno/ Tech-House </p>
+                            <p>Cosa sentirai in pista: Bob Sinclar, Ricky Martin, Daft Punk,Outkast, Piotta, Mik, Caparezza,883, Cartoon, Gigi d'Agostino e tanti altri.. che hanno fatto la storia degli anni 90 e 2000. Non dimenticarti di portare...</p>
+                            <button type="button" class="btn btn-type-3 color-transparent">Continua a leggera</button>
+                        </div>
+                      </div>
+                    </div>
+                  </div> 
+                </div>
+                <!-- slide end -->
+                                <!-- SLIDE Content-->
+                <div> 
+                  <div class="row">
+                    <div class="col-md-6 eq-with-black">
+                      <div class="row">
+                        <div class="col-md-12 eq-sld-poster">
+                            <img src="img/event.png">
+                        </div>
+                      </div>
+                      <div class="row eq-sld-right">
+                          <div class="col-md-6 eq-sld-price">
+                              <span class="span-currency">a partile da</span><h3 class="eq-sc-title-span"><i class="fa fa-eur"></i> 22.0 </h3>
+                          </div>
+                          <div class="col-md-6 eq-sld-btn">
+                            <button type="button" class="btn btn-type-1 color-primary">Acquista</button>
+                          </div>
+                        </div>                        
+                    </div>
+                    <div class="col-md-6">
+                      <div class="eq-sld-eventdesc">
+                        <div class="eq-sld-eventdesc-title">
+                          <h4> <span class="eq-event-name">Metroman</span> <span class="eq-event-name">Roberto Genovese DJ </span></h4>
+                        </div>
+                        <div class="eq-sld-eventdesc-parag">
+                            <p>Balla al ritmo dei successi anni 90 e 2000. Muovi i fianchi e divertiti con i successi degli anni passati... Per veri nostalgici!
+                              PISTA 2 HOT IN HERE /hip hop/reggaeton/trap
+                              PISTA 3 Techno/ Tech-House </p>
+                            <p>Cosa sentirai in pista: Bob Sinclar, Ricky Martin, Daft Punk,Outkast, Piotta, Mik, Caparezza,883, Cartoon, Gigi d'Agostino e tanti altri.. che hanno fatto la storia degli anni 90 e 2000. Non dimenticarti di portare...</p>
+                            <button type="button" class="btn btn-type-3 color-transparent">Continua a leggera</button>
+                        </div>
+                      </div>
+                    </div>
+                  </div> 
+                </div>
+                <!-- slide end -->
+                                <!-- SLIDE Content-->
+                                <div> 
+                                    <div class="row">
+                                      <div class="col-md-6 eq-with-black">
+                                        <div class="row">
+                                          <div class="col-md-12 eq-sld-poster">
+                                              <img src="img/event.png">
+                                          </div>
+                                        </div>
+                                        <div class="row eq-sld-right">
+                                            <div class="col-md-6 eq-sld-price">
+                                                <span class="span-currency">a partile da</span><h3 class="eq-sc-title-span"><i class="fa fa-eur"></i> 22.0 </h3>
+                                            </div>
+                                            <div class="col-md-6 eq-sld-btn">
+                                              <button type="button" class="btn btn-type-1 color-primary">Acquista</button>
+                                            </div>
+                                          </div>                        
+                                      </div>
+                                      <div class="col-md-6">
+                                        <div class="eq-sld-eventdesc">
+                                          <div class="eq-sld-eventdesc-title">
+                                            <h4> <span class="eq-event-name">Metroman</span> <span class="eq-event-name">Roberto Genovese DJ </span></h4>
+                                          </div>
+                                          <div class="eq-sld-eventdesc-parag">
+                                              <p>Balla al ritmo dei successi anni 90 e 2000. Muovi i fianchi e divertiti con i successi degli anni passati... Per veri nostalgici!
+                                                PISTA 2 HOT IN HERE /hip hop/reggaeton/trap
+                                                PISTA 3 Techno/ Tech-House </p>
+                                              <p>Cosa sentirai in pista: Bob Sinclar, Ricky Martin, Daft Punk,Outkast, Piotta, Mik, Caparezza,883, Cartoon, Gigi d'Agostino e tanti altri.. che hanno fatto la storia degli anni 90 e 2000. Non dimenticarti di portare...</p>
+                                              <button type="button" class="btn btn-type-3 color-transparent">Continua a leggera</button>
+                                          </div>
+                                        </div>
+                                      </div>
+                                    </div> 
+                                  </div>
+                                  <!-- slide end -->
+              </div>
+            </div>
+        </div>
+      </div>
+    </div>
+
+  </div>
+
+  <!-- CATEGORIE SECTION -->
+
+  <div id="container2">
+      <div class="section-heading">
+        <div class="row">
+            <div class="col-md-12">
+               <h2 class="section-title"> CATEGORIE </h2>
+            </div>
+        </div>
+      </div>
+
+      <div class="owl-two owl-carousel owl-theme">
+        <div class="col-md-12">
+          <div class="box-item eq-shadow">
+            <div class="category-image">
+              <img src="img/musica.png" width="100%" alt="category-title">
+            </div>
+            <div class="category-title-container">
+              <span class="eq-sc-title-span">MUSICA</span>
+            </div>
+          </div>
+        </div>
+      </div>
+      
+  </div>
+
+    <!-- ASSISTENZA SECTION -->
+
+    <div id="container2">
+      <div class="section-heading">
+        <div class="row">
+            <div class="col-md-12">
+               <h2 class="section-title"> ASSISTENZA </h2>
+            </div>
+        </div>
+      </div>
+      <div class="col-md-12">
+        <div class="row eq-blank">
+          <div class="col-md-8 small-box-left eq-shadow">
+            <div class="row">
+                <div class="col-md-4 box-icon">
+                    <span class="box-icon-text"><i class="fa fa-question"></i></span>
+                  </div>
+                  <div class="col-md-8 box-desc">
+                    <h3 class="eq-sc-title-span"> DUBBI... </h3>
+                    Il nostro centro assistenza è a tua disposizione per qualunque cosa. Problemi tencici sul sito? Sei un organizzatore e hai bisogno di una mano per inserire un evento? Sei un privato e non ti senti sicuro nell’acquisto di Equiticket?
+                  </div>
+            </div>
+
+          </div>
+          <div class="col-md-4 small-box-right eq-shadow">
+              <h3 class="eq-sc-title-span text-left"> ... O DOMANDE? </h3>
+              <button type="button" class="btn btn-type-4 color-primary"> Contattaci </button>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <!-- TESTIMONIANZE SECTION -->
+
+    <div id="container2">
+      <div class="section-heading">
+        <div class="row">
+            <div class="col-md-12">
+               <h2 class="section-title"> TESTIMONIANZE </h2>
+            </div>
+        </div>
+      </div>
+      <div class="owl-testimonianze owl-carousel owl-theme">
+        <div class="col-md-12">
+            <div class="box-item eq-shadow">
+                <div class="testimonianze-container">
+                  <q class="blockquote">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </q>
+                  <span class="testimonianze-name">Riccardo M</span>
+                </div>
+            </div>
+        </div>
+      </div>
+      
+    </div>
+
+      <!-- PARLANO SECTION -->
+
+      <div id="container2">
+        <div class="section-heading">
+          <div class="row">
+              <div class="col-md-12">
+                 <h2 class="section-title"> PARLANO DI NOI </h2>
+              </div>
+          </div>
+        </div>
+        <div class="owl-parlano owl-carousel owl-theme">
+            <div class="item eq-shadow" style="width:250px; height: 200px; background-color: grey"></div>
+            <div class="item eq-shadow" style="width:250px; height: 200px; background-color: grey"></div>
+            <div class="item eq-shadow" style="width:250px; height: 200px; background-color: grey"></div>
+            <div class="item eq-shadow" style="width:250px; height: 200px; background-color: grey"></div>
+            <div class="item eq-shadow" style="width:250px; height: 200px; background-color: grey"></div>
+            <div class="item eq-shadow" style="width:250px; height: 200px; background-color: grey"></div>
+        </div>
+        
+    </div>
+
+<!-- Footer -->
+<footer id="footer-1">
+  <div class="footer-container">
+    <div class="row">
+        <div class="col-md-6 footerpd">
+            <div class="row">
+              <div class="col-md-6 footer-left">
+                  <a class="footer-brand" href="#"><img class="logo" src="img/logo.png"></a>
+              </div>
+              <div class="col-md-6 footer-right">
+                  ssfsdfs
+              </div>
+              <div class="col-md-6">
+                  ssfsdfs
+              </div>
+            </div>
+        </div>
+        <div class="col-md-6 footerpd">
+            <div class="row">
+              <div class="col-md-6">
+                adadasdas
+              </div>
+              <div class="col-md-6">
+                adadasdas
+              </div>
+            </div>
+            <div class="row">
+              <div class="col-md-12">
+                cxvxvcxvxvxcvxv xcvxc vcxvxc vxcv xcvcxvc
+              </div>
+            </div>
+        </div>
+    </div>
+  </div>
+</footer>
+
+  <!-- Js -->
+  <script src="js/jquery.slim.min.js"></script>
+  <script src="js/bootstrap.bundle.min.js"></script>
+  <script src="js/jquery.min.js"></script>
+  <script src="js/owl.carousel.min.js"></script>
+  <script src="js/slides.js"></script>
+
+</body>
+
+</html>
