a148e7fca69595cdfa42b446674e4b90db37451a
Rodgers Sande <rd_sande@yahoo.com>
2019-08-20 03:06:47 -0700

    Mobile  navigation styles

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -49,7 +49,9 @@
       <div class="container">
         <a class="navbar-brand" href="#"><img class="logo" src="{{asset('img/logo.png')}}"></a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon menu-toggler"></span>
+          <span class="navbar-toggler-icon menu-toggler">
+            <i class="fa fa-bars nav-bars"></i>
+          </span>
         </button>
         <div class="collapse navbar-collapse" id="navbarResponsive">
           <ul class="navbar-nav ml-auto">
